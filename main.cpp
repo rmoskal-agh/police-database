115fe3d459c3938fd34c446d8cfe84cb568ef2f5
rmoskal-agh <rmoskal@student.agh.edu.pl>
2026-01-13 18:18:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <limits>
 #include "Person.hpp"
@@ -6,14 +5,13 @@
 #include "Suspect.hpp"
 #include "PoliceDatabase.hpp"
 
-// Funkcja pomocnicza do czyszczenia bufora wejścia
 void clearInput() {
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
 
 int main() {
-    // Inicjalizacja bazy danych (przestrzeń nazw PG)
+    
     rmpg::PoliceDatabase db("police_db.txt");
 
     std::cout << "Welcome to Police Records (System provided by R. Moskal & P. Grabowski)\n";
@@ -24,8 +22,8 @@ int main() {
         std::cout << "3. Add a suspect\n";
         std::cout << "4. Edit record by ID\n";
         std::cout << "5. Remove record by ID\n";
-        std::cout << "6. Save and close\n";
-
+        std::cout << "6. Show Statistics\n";       
+        std::cout << "7. Save and close\n";        
         std::cout << "Choice: ";
 
         int choice;
@@ -34,11 +32,10 @@ int main() {
             continue;
         }
 
-        if (choice == 6) break;
+        if (choice == 7) break; 
 
         try {
             if (choice == 1) {
-                // Wyświetlanie danych zwróconych przez klasę (nie klasa drukuje!)
                 std::cout << db.getAllRecords();
             }
             else if (choice == 2) {
@@ -52,44 +49,39 @@ int main() {
                 std::cout << "Rank: "; std::cin >> rank;
                 std::cout << "Badge ID: "; std::cin >> badge;
 
-                // Tworzenie obiektu dynamicznie (polimorfizm)
                 rmpg::Officer* newOfficer = new rmpg::Officer(fName, lName, age, id, rank, badge);
                 
-                // Demonstracja operatora RM (awans)
                 std::cout << "Do you want to promote this person? (1-Yes, 0-No): ";
                 int promote;
                 std::cin >> promote;
                 if(promote) {
-                    ++(*newOfficer); // Operator ++
+                    ++(*newOfficer); 
                 }
 
-                db += newOfficer; // Użycie operatora += z klasy PG
+                db += newOfficer; 
                 std::cout << "A police officer has been added.\n";
             }
             else if (choice == 3) {
                 std::string fName, lName;
                 int age, id;
                 bool dangerous;
-                            
+                
                 std::cout << "First name: "; std::cin >> fName;
                 std::cout << "Last name: "; std::cin >> lName;
                 std::cout << "Age: "; std::cin >> age;
                 std::cout << "ID: "; std::cin >> id;
-            
+                
                 rmpg::Suspect::printCrimeMenu();
-            
                 int crimeChoice;
                 std::cout << "Choice: ";
                 std::cin >> crimeChoice;
-            
                 std::string crime = rmpg::Suspect::crimeFromChoice(crimeChoice);
-            
+
                 std::cout << "Are they dangerous (1-Yes, 0-No): ";
                 std::cin >> dangerous;
-            
-                rmpg::Suspect* newSuspect =
-                    new rmpg::Suspect(fName, lName, age, id, crime, dangerous);
-            
+
+                rmpg::Suspect* newSuspect = new rmpg::Suspect(fName, lName, age, id, crime, dangerous);
+                
                 db.addPerson(newSuspect);
                 std::cout << "A suspect has been added.\n";
             }
@@ -97,27 +89,29 @@ int main() {
                 int id;
                 std::cout << "Enter ID to edit: ";
                 std::cin >> id;
-            
-                if (!db.editById(id))
-                    std::cout << "Record not found.\n";
+                if (!db.editById(id)) {
+                    std::cout << "Record not found or edit cancelled.\n";
+                }
             }
             else if (choice == 5) {
                 int id;
                 std::cout << "Enter ID to remove: ";
                 std::cin >> id;
-            
-                if (db.removeById(id))
+                if (db.removeById(id)) {
                     std::cout << "Record removed.\n";
-                else
+                } else {
                     std::cout << "Record not found.\n";
+                }
             }
-
+            else if (choice == 6) {
+                db.printStatistics();
+            }
         }
         catch (const std::exception& e) {
             std::cerr << "[Error]: " << e.what() << "\n";
         }
     }
 
-    std::cout << "Data has been saved succesfully. End of session.\n";
+    std::cout << "Data has been saved successfully. End of session.\n";
     return 0;
 }
